6db0d422bda23df4d35fd4ad91b3d611537a9fd3
Delta_Zheng <Delta_Zheng@wistronits.com>
2022-10-19 17:59:56 +0800

    删除面包屑

--- src/pages/cms/cms.page.tsx
+++ src/pages/cms/cms.page.tsx
@@ -82,12 +82,12 @@ export default function CMS() {
         <Layout className="site-layout">
           <Header className="site-layout-background" style={{ padding: 0 }} />
           <Content style={{ margin: "0 16px" }}>
-            <Breadcrumb style={{ margin: "16px 0" }}>
+            {/* <Breadcrumb style={{ margin: "16px 0" }}>
               <Breadcrumb.Item>User</Breadcrumb.Item>
               <Breadcrumb.Item>Bill</Breadcrumb.Item>
-            </Breadcrumb>
+            </Breadcrumb> */}
             <div
-              className="site-layout-background"
+              className="site-layout-background mt-4 w-full h-full"
               style={{ padding: 24, minHeight: 360 }}
             >
               <Outlet />

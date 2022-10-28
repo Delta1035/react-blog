a88e39e0aa3dae612e615040eb493cf56f54fba4
Delta_Zheng <Delta_Zheng@wistronits.com>
2022-10-28 17:54:28 +0800

    修改vite 配置

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./node_modules/"),
-      src: path.resolve(__dirname, "./src"),
+      "src": path.resolve(__dirname, "./src"),
     },
   },
 });

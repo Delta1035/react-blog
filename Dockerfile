c6e4ab80f40ce9b2bc41854c7c7bbec3befa39b8
Delta_Zheng <Delta_Zheng@wistronits.com>
2022-12-28 15:04:55 +0800

    增加docker配置

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,9 @@
-From nginx
-Run echo '来自本地构建的nginx镜像' > /usr/share/nginx/html/index.html
+FROM nginx:1.23.3
+LABEL auth_email="528491526@qq.com" 
+LABEL auth_name="delta" 
+# RUN echo '来自本地构建的nginx镜像' > /usr/share/nginx/html/index.html
+# 将dist目录的文件复制到html文件
+COPY ./dist/ /usr/share/nginx/html
+
+# 复制nginx的配置文件
+COPY ./nginx-custom.conf /etc/nginx/conf.d/nginx-custom.conf
--- src/blog/home/home.page.tsx
+++ src/blog/home/home.page.tsx
@@ -1,8 +1,7 @@
-import { FullContainer } from "@src/components/full-container.component";
-import React, { Children } from "react";
+import React from "react";
 
-const Home: React.FC<{ children: React.ReactNode }> = ({children}) => {
-  return <FullContainer>{Children}</FullContainer>;
+const Home: React.FC = ({}) => {
+  return <div>{}</div>;
 };
 
 export default Home;

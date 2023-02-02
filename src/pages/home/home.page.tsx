672bbbe5dceaed81f0d4fb64ad73b751ffe2c5bc
Delta_Zheng <Delta_Zheng@wistronits.com>
2023-02-02 09:30:07 +0800

    解决文章列表margin导致左右溢出问题

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,3 +1,12 @@
+/*
+ * @Author: Delta_Zheng Delta_Zheng@wistronits.com
+ * @Date: 2022-12-23 13:12:41
+ * @LastEditors: Delta_Zheng Delta_Zheng@wistronits.com
+ * @LastEditTime: 2023-02-02 09:25:29
+ * @FilePath: \react-blog\src\pages\home\home.page.tsx
+ * @Description: 
+ * 
+ */
 import ArticleList from "@src/components/articleList";
 import Footer from "@src/components/footer";
 import React, { Suspense } from "react";
@@ -11,7 +20,7 @@ const Home: React.FC = ({}) => {
     <div className="home">
       <header>
         <img src={delta} alt="delta"></img>
-        <h3>踩坑备份博客</h3>
+        <h3>学习记录踩坑备份博客</h3>
       </header>
       <div className="content">
         <aside></aside>
